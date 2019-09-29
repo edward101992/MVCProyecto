fbb4da0b9e0b0aed1ba51b2441f7c4ef82b1ab33
Edward <edwardconw@gmail.com>
2019-09-29 13:35:57 -0500

    Creacion de Modelo

--- src/main/java/modelo/Modelo.java
+++ src/main/java/modelo/Modelo.java
@@ -11,4 +11,54 @@ package modelo;
  */
 public class Modelo {
     
+    private double peso;
+    private double dolar;
+    private double euro;
+    private double yen;
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getDolar() {
+        return dolar;
+    }
+
+    public void setDolar(double dolar) {
+        this.dolar = dolar;
+    }
+
+    public double getEuro() {
+        return euro;
+    }
+
+    public void setEuro(double euro) {
+        this.euro = euro;
+    }
+
+    public double getYen() {
+        return yen;
+    }
+
+    public void setYen(double yen) {
+        this.yen = yen;
+    }
+    
+     public double convertirDolar(){
+        
+        this.dolar = this.peso * 0.00029;
+        return peso;
+    }
+    
+    public double convertirYen(){
+    
+        this.yen = this.peso * 0.031;
+        return yen;
+    }
+    
+    
 }
